6acedbc6bc1459a8d506264191f021b205d39a52
Raja s <rajaaltus@gmail.com>
2020-02-21 19:31:21 +0700

    initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# virginia
+
+> Virginia Enterprises
+
+> Website for VIRGINIA ENTERPRISES | IMPORT | EXPORT | TRADING Company, Vientiane, Laos
---
+++ nuxt.config.js
@@ -0,0 +1,74 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Sarabun:400,500,600,700|Rubik:300,400,500' },
+      { rel: 'stylesheet', href: '/css/pe-icon-7-stroke.css' },
+      { rel: 'stylesheet', href: '/css/materialdesignicons.min.css' },
+      { rel: 'stylesheet', href: '/css/bootstrap.min.css' },
+      { rel: 'stylesheet', href: '/css/magnific-popup.css' },
+      { rel: 'stylesheet', href: '/css/style.css' },
+      { rel: 'stylesheet', href: '/css/colors/cyan.css', id: "color-opt"}
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    {src: '~/plugins/vue-youtube', ssr: false}
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+    'vue-scrollto/nuxt',
+    'bootstrap-vue/nuxt',
+  ],
+  
+  
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
